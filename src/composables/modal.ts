4c3cd399daf3c734984bb8dac8142f6d8f19b9d3
Dusan <dusan.trtica@hotmail.com>
2023-04-17 22:13:48 +0200

    refactored modal so that it uses components

--- src/composables/modal.ts
+++ src/composables/modal.ts
@@ -1,10 +1,22 @@
-import { ref } from 'vue';
+import { ref, shallowRef } from 'vue';
+import SignupFormVue from '../components/SignupForm.vue';
+import HelloWorldVue from '../components/HelloWorld.vue';
+
 const show = ref(false);
+
+const component = shallowRef();
 
 export function useModal() {
     return {
         show,
-        showModal: () => show.value = true,
+        component,
+        showModal: (type: 'signUp' | 'signIn') => {
+            show.value = true
+            switch(type) {
+                case 'signIn': return component.value = HelloWorldVue;
+                case 'signUp': return component.value = SignupFormVue;
+            }
+        },
         hideModal: () => show.value = false
     }
 }
